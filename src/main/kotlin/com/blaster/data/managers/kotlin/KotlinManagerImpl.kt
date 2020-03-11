1ed548ffdd740367b765ada3ed13283c4fcbe112
Grigory Zozulin <Grigory.Zozulin@soti.net>
2020-03-11 14:49:17 -0400

    parsers are shared

--- src/main/kotlin/com/blaster/data/managers/kotlin/KotlinManagerImpl.kt
+++ src/main/kotlin/com/blaster/data/managers/kotlin/KotlinManagerImpl.kt
@@ -1,52 +1,83 @@
 package com.blaster.data.managers.kotlin
 
 import com.blaster.business.Location
-import org.antlr.v4.runtime.CharStreams
-import org.antlr.v4.runtime.CommonTokenStream
-import org.antlr.v4.runtime.ParserRuleContext
-import org.antlr.v4.runtime.Token
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import org.antlr.v4.runtime.*
+
+import java.io.File
 import java.net.URL
 
+typealias ClassContext = KotlinParser.ClassDeclarationContext
+typealias FunctionContext = KotlinParser.FunctionDeclarationContext
+typealias PropertyContext = KotlinParser.PropertyDeclarationContext
+
+private data class CacheEntry(var parser: KotlinParser? = null, val mutex: Mutex = Mutex())
+
 class KotlinManagerImpl : KotlinManager {
+    private val cache = mutableMapOf<File, CacheEntry>()
+    private fun useParser(file: File, action: (parser: KotlinParser, stream: CommonTokenStream) -> String): String {
+        var entry: CacheEntry
+        synchronized(cache) {
+           if (!cache.containsKey(file)) {
+               cache[file] = CacheEntry()
+           }
+            entry = cache[file]!!
+        }
+        return runBlocking {
+            entry.mutex.withLock {
+                if (entry.parser == null) {
+                    entry.parser = KotlinParser(CommonTokenStream(KotlinLexer(CharStreams.fromFileName(file.absolutePath))))
+                }
+                val parser = entry.parser!!
+                val stream = parser.tokenStream as CommonTokenStream
+                parser.reset()
+                return@runBlocking action.invoke(parser, stream)
+            }
+        }
+    }
+
     override fun extractDefinition(location: Location): String {
-        val stream = CommonTokenStream(KotlinLexer(CharStreams.fromFileName(location.file.absolutePath)))
-        val definition = locateCode(stream, location)
-        location.url = URL(location.url.toString() + "#L${definition.start.line}") // todo: ugly shortcut
-        val firstToken = findFirstToken(stream, definition)
-        val lastToken = definition.stop.tokenIndex
-        val tokens = stream.get(firstToken, lastToken)
-        return tokensToText(tokens)
+        return useParser(location.file) { parser, stream ->
+            val definition = locateCode(parser, location)
+            location.url = URL(location.url.toString() + "#L${definition.start.line}") // todo: ugly shortcut
+            val firstToken = findFirstToken(stream, definition)
+            val lastToken = definition.stop.tokenIndex
+            val tokens = stream.get(firstToken, lastToken)
+            return@useParser tokensToText(tokens)
+        }
     }
 
     override fun extractDeclaration(location: Location): String {
-        val stream = CommonTokenStream(KotlinLexer(CharStreams.fromFileName(location.file.absolutePath)))
-        val declaration = locateCode(stream, location)
-        location.url = URL(location.url.toString() + "#L${declaration.start.line}") // todo: ugly shortcut
-        val firstToken = findFirstToken(stream, declaration)
-        val lastToken = findLastToken(stream, declaration)
-        val tokens = stream.get(firstToken, lastToken)
-        return tokensToText(tokens)
+        return useParser(location.file) { parser, stream ->
+            val declaration = locateCode(parser, location)
+            location.url = URL(location.url.toString() + "#L${declaration.start.line}") // todo: ugly shortcut
+            val firstToken = findFirstToken(stream, declaration)
+            val lastToken = findLastToken(stream, declaration)
+            val tokens = stream.get(firstToken, lastToken)
+            return@useParser tokensToText(tokens)
+        }
     }
 
-    private fun locateCode(stream: CommonTokenStream, location: Location): ParserRuleContext {
-        val parser = KotlinParser(stream)
+    private fun locateCode(parser: KotlinParser, location: Location): ParserRuleContext {
         val declarations = mutableListOf<ParserRuleContext>()
         val visitor = object : KotlinParserBaseVisitor<Unit>() {
-            override fun visitClassDeclaration(ctx: KotlinParser.ClassDeclarationContext?) {
+            override fun visitClassDeclaration(ctx: ClassContext?) {
                 super.visitClassDeclaration(ctx)
                 if (ctx!!.simpleIdentifier().text == location.identifier) {
                     declarations.add(ctx)
                 }
             }
 
-            override fun visitFunctionDeclaration(ctx: KotlinParser.FunctionDeclarationContext?) {
+            override fun visitFunctionDeclaration(ctx: FunctionContext?) {
                 super.visitFunctionDeclaration(ctx)
                 if (ctx!!.identifier().text == location.identifier) {
                     declarations.add(ctx)
                 }
             }
 
-            override fun visitPropertyDeclaration(ctx: KotlinParser.PropertyDeclarationContext?) {
+            override fun visitPropertyDeclaration(ctx: PropertyContext?) {
                 super.visitPropertyDeclaration(ctx)
                 val variableDeclaration = ctx!!.variableDeclaration() ?: return
                 if (variableDeclaration.simpleIdentifier().text == location.identifier) {
@@ -70,21 +101,21 @@ class KotlinManagerImpl : KotlinManager {
 
     private fun findLastToken(tokenStream: CommonTokenStream, decl: ParserRuleContext): Int {
         return when (decl) {
-            is KotlinParser.ClassDeclarationContext -> {
+            is ClassContext -> {
                 if (decl.classBody() != null) {
                     decl.classBody().start.tokenIndex - 1
                 } else {
                     decl.stop.tokenIndex // class have no body
                 }
             }
-            is KotlinParser.FunctionDeclarationContext -> {
+            is FunctionContext -> {
                 if (decl.functionBody() != null) {
                     decl.functionBody().start.tokenIndex - 1
                 } else {
                     decl.stop.tokenIndex // function have no body
                 }
             }
-            is KotlinParser.PropertyDeclarationContext -> {
+            is PropertyContext -> {
                 if (decl.text.contains("object")) {
                     findBodyStart(tokenStream, decl).tokenIndex - 2 // for objects before body begins
                 } else {
